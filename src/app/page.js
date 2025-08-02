288812f3c03907b09db2a14c3ef37b6bb6ec48f5
Victor Kauan <victorkauan.dev@gmail.com>
2025-08-02 20:31:54 -0300

    :sparkles: feat: Get posts from API

--- src/app/page.js
+++ src/app/page.js
@@ -1,24 +1,21 @@
 import { PostCard } from "@/components/PostCard";
 
-const post = {
-    id: 1,
-    cover: "https://raw.githubusercontent.com/viniciosneves/code-connect-assets/main/posts/introducao-ao-react.png",
-    title: "Introdução ao React",
-    slug: "introducao-ao-react",
-    body: "Neste post, vamos explorar os conceitos básicos do React, uma biblioteca JavaScript para construir interfaces de usuário. Vamos cobrir componentes, JSX e estados.",
-    markdown: "```javascript\nfunction HelloComponent() {\n  return <h1>Hello, world!</h1>;\n}\n```",
-    author: {
-        id: 101,
-        name: "Ana Beatriz",
-        username: "anabeatriz_dev",
-        avatar: "https://raw.githubusercontent.com/viniciosneves/code-connect-assets/main/authors/anabeatriz_dev.png"
-    }
+async function getAllPosts() {
+  const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/posts`)
+
+  if (!response.ok) {
+    console.log('Ops, alguma coisa correu mal!')
+  }
+
+  return response.json()
 }
 
-export default function Home() {
+export default async function Home() {
+  const posts = await getAllPosts()
+
   return (
     <main>
-      <PostCard post={post} />
+      {posts.map(post => <PostCard key={post.id} post={post} />)}
     </main>
   );
 }
