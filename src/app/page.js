1af9c2391b71e5f95663e244ff83b536f10b61d5
Victor Kauan <victorkauan.dev@gmail.com>
2025-08-02 21:17:31 -0300

    :sparkles: feat: Implement post list pagination

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,12 @@
 import logger from "@/logger";
 import { PostCard } from "@/components/PostCard";
 import styles from "@/app/page.module.css"
+import Link from "next/link";
 
-async function getAllPosts() {
-  const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/posts`)
+const DEFAULT_PER_PAGE = 6
+
+async function getAllPosts(page) {
+  const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/posts?_page=${page}&_per_page=${DEFAULT_PER_PAGE}`)
 
   if (!response.ok) {
     logger.error("Ops, alguma coisa correu mal!")
@@ -14,12 +17,17 @@ async function getAllPosts() {
   return response.json()
 }
 
-export default async function Home() {
-  const posts = await getAllPosts()
+export default async function Home({ searchParams }) {
+  const currentPage = searchParams?.page ?? 1
+  const { data: posts, prev, next } = await getAllPosts(currentPage)
 
   return (
     <main className={styles.grid}>
       {posts.map(post => <PostCard key={post.id} post={post} />)}
+      <div className={styles.links}>
+        {prev && <Link href={`/?page=${prev}`}>Página anterior</Link>}
+        {next && <Link href={`/?page=${next}`}>Próxima página</Link>}
+      </div>
     </main>
   );
 }
