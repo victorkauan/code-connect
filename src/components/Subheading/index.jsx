cc32ac5619a95a45f14da9d8030243bbeedb12ae
Victor Kauan <victorkauan.dev@gmail.com>
2025-08-12 15:05:05 -0300

    :sparkles: feat: Create subheading component

---
+++ src/components/Subheading/index.jsx
@@ -0,0 +1,7 @@
+import styles from "@/components/Subheading/subheading.module.css";
+
+export const Subheading = () => {
+  return (
+    <h2 className={styles.subheading}>Deixe seu comentário sobre o post:</h2>
+  );
+};
