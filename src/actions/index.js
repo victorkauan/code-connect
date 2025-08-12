c99936dcada8fd8d2922d73d7d74c17cdc0fe770
Victor Kauan <victorkauan.dev@gmail.com>
2025-08-12 11:03:17 -0300

    :sparkles: feat: Create increment thumbs up action

---
+++ src/actions/index.js
@@ -0,0 +1,14 @@
+"use server";
+
+import { revalidatePath } from "next/cache";
+import db from "../../prisma/db";
+
+export async function incrementThumbsUp(post) {
+  await db.post.update({
+    where: { id: post.id },
+    data: { likes: { increment: 1 } },
+  });
+
+  revalidatePath("/");
+  revalidatePath(`/${post.slug}`);
+}
