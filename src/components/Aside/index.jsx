4e32d2322b7d7bf5bee12760816c7517244813e4
Victor Kauan <victorkauan.dev@gmail.com>
2025-08-11 22:05:24 -0300

    :sparkles: feat: Create home link in Aside component

--- src/components/Aside/index.jsx
+++ src/components/Aside/index.jsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import Image from "next/image";
 import styles from "@/components/Aside/aside.module.css";
 import logo from "@/assets/logo.png";
@@ -5,7 +6,9 @@ import logo from "@/assets/logo.png";
 export const Aside = () => {
   return (
     <aside className={styles.aside}>
-      <Image src={logo} alt="Logo da Code Connect" />
+      <Link href="/">
+        <Image src={logo} alt="Logo da Code Connect" />
+      </Link>
     </aside>
   );
 };
